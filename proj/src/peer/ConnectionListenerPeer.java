8f02129b747511754d5ce0099d133838e90ad746
André Pires <andre.r.o.pires@gmail.com>
2015-05-21 23:21:03 +0000

    check online users

--- proj/src/peer/ConnectionListenerPeer.java
+++ proj/src/peer/ConnectionListenerPeer.java
@@ -4,81 +4,71 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.lang.reflect.Type;
 import java.security.PrivilegedActionException;
 import java.security.Security;
-import java.util.ArrayList;
 
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 
-
-
-
-
-import user.User;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.sun.net.ssl.internal.ssl.Provider;
 
 import extra.Tools;
 
 public class ConnectionListenerPeer extends Thread{
 	private PeerNew mainThread;
-	
+
 	public ConnectionListenerPeer(PeerNew peer){
 		this.mainThread = peer;
 	}
-	
+
 	@Override
 	public void run() {
 		{
 			// Registering the JSSE provider
 			Security.addProvider(new Provider());
-	
+
 			//Specifying the Keystore details
 			System.setProperty("javax.net.ssl.keyStore","..\\certificates\\peer\\keystore");
 			System.setProperty("javax.net.ssl.keyStorePassword","serverkey");
-	
+
 			// Enable debugging to view the handshake and communication which happens between the SSLClient and the SSLServer
 			// System.setProperty("javax.net.debug","all");
 		}
-		
+
 		SSLServerSocket sslServerSocket = null;
 		SSLSocket sslSocket = null ;
-		
+
 		while(true){
 			try {
-				
+
 				// Initialize the Server Socket
 				SSLServerSocketFactory sslServerSocketfactory = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
 				sslServerSocket = (SSLServerSocket)sslServerSocketfactory.createServerSocket(this.mainThread.getLocalUser().getPort());
 				sslSocket = (SSLSocket)sslServerSocket.accept();
-				
+
 				PrintWriter out = new PrintWriter(sslSocket.getOutputStream(), true); //vai responder por aqui
-		        BufferedReader in = new BufferedReader(new InputStreamReader(sslSocket.getInputStream())); //lê daqui
-		        
-		        String inputLine = in.readLine();
-		        inputLine += in.readLine();
-		        inputLine+= "\r\n\r\n";
-		        inputLine += in.readLine();
-		        
-		        System.out.println("message received: \n	"+inputLine);
-		        String origin_ip = sslSocket.getInetAddress().getHostAddress();
-		        //PROCESS RECEIVED MESSAGE
-		        System.out.println("	from: "+origin_ip);
-		        String responseMessage = this.parseReceivedMessage(inputLine);
-		        //SEND RESPONSE
-		        out.println(responseMessage);
-		        System.out.println("Answer sent!");
-		        // Close the streams and the socket
-		        out.close();
-		        in.close();
-		        sslSocket.close();
-		        sslServerSocket.close();	
-				
+				BufferedReader in = new BufferedReader(new InputStreamReader(sslSocket.getInputStream())); //lê daqui
+
+				String inputLine = in.readLine();
+				inputLine += in.readLine();
+				inputLine+= "\r\n\r\n";
+				inputLine += in.readLine();
+
+				System.out.println("message received: \n	"+inputLine);
+				String origin_ip = sslSocket.getInetAddress().getHostAddress();
+				//PROCESS RECEIVED MESSAGE
+				System.out.println("	from: "+origin_ip);
+				String responseMessage = this.parseReceivedMessage(inputLine);
+				//SEND RESPONSE
+				out.println(responseMessage);
+				System.out.println("Answer sent!");
+				// Close the streams and the socket
+				out.close();
+				in.close();
+				sslSocket.close();
+				sslServerSocket.close();	
+
 			}
 			catch(Exception exp)
 			{
@@ -86,20 +76,18 @@ public class ConnectionListenerPeer extends Thread{
 				System.out.println(" Priv exp --- " + priexp.getMessage());
 				System.out.println(" Exception occurred .... " +exp);
 				//exp.printStackTrace();
-				
-		        try {
+
+				try {
 					sslSocket.close();
 					sslServerSocket.close();
 				} catch (IOException e) {
 					System.out.println("erro no while read server");
 					//e.printStackTrace();
-				}
-		        
+				}	        
 			}
 		}
-		
 	}
-	
+
 	/**
 	 * parses received message, executes action, and generates response.
 	 * @param message
@@ -107,20 +95,16 @@ public class ConnectionListenerPeer extends Thread{
 	 */
 	public String parseReceivedMessage(String message){
 		String[] messageHeadParts = Tools.getHead(message).split(" +");
-		int user_id;
-		Gson gson = new Gson();;
-		
+		//int user_id;
+		//Gson gson = new Gson();;
+
 		switch(messageHeadParts[0]) {
 		case "ISONLINE":
 			return Tools.generateMessage("OK");
-			
+
 		default:
 			break;				
 		}
 		return null;
-	}
-	
-	
-	
-	
+	}	
 }
--- proj/src/peer/PeerNew.java
+++ proj/src/peer/PeerNew.java
@@ -1,26 +1,14 @@
 package peer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.lang.reflect.Type;
-import java.net.ConnectException;
-import java.net.SocketTimeoutException;
-import java.security.Security;
 import java.util.ArrayList;
 
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
-
-import server.ConnectionListenerServer;
 import ui.loginFrame.LoginFrame;
 import ui.mainFrame.GUI;
 import user.User;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.sun.net.ssl.internal.ssl.Provider;
 
 import extra.Tools;
 
@@ -28,121 +16,40 @@ public class PeerNew {
 	private User localUser;
 	private String serverAddress = "localhost";
 	private int serverPort = 16500; 
-	
+
 	ArrayList<User> friends;
-	
-	public PeerNew(){
-		
-		
-	}
-	
-	
-	private static int connection_try_number = 0;
-	
+
+	public PeerNew() {}
+
 	private LoginFrame loginFrame;
 	private GUI mainFrame;
-	
-	/**
-	 * 
-	 * @param msg
-	 * @param ip_dest
-	 * @param port_dest
-	 * @return response from other peer/server
-	 */
-	public String sendMessage(String msg, String ip_dest, int port_dest){
-		{
-			// Registering the JSSE provider
-			Security.addProvider(new Provider());
-		}
-		
-		int timeout = 5000; //timeout in miliseconds
-		
-		SSLSocketFactory sslsocketfactory = (SSLSocketFactory)SSLSocketFactory.getDefault();
-		SSLSocket sslSocket;
-		
-		PrintWriter out = null;
-        BufferedReader in = null;
-        String response = null;
-        
-		try {
-			sslSocket = (SSLSocket)sslsocketfactory.createSocket(ip_dest,port_dest);
-			sslSocket.setSoTimeout(timeout);
-			
-			// Initializing the streams for Communication with the Server
-         	out = new PrintWriter(sslSocket.getOutputStream(), true);
-         	in = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
 
-			//SEND MESSAGE
-         	out.println(msg);
-			
-         	//GET RESPONSE 
-			response = in.readLine();
-			response += in.readLine(); //TODO (isto está assim hardcoded pq o 
-			response += "\r\n\r\n"+in.readLine(); //readLine lê até ao \r\n apenas)
-	
-			//PARSE RESPONSE
-			String origin_ip = sslSocket.getInetAddress().getHostAddress();
-			//System.out.println("response: " + response + "#" + origin_ip);
-
-			// Closing the Streams and the Socket
-			out.close();
-			in.close();
-			sslSocket.close();		
-			
-		} 
-		catch (SocketTimeoutException e){
-			System.out.println("timeout");
-			if (++connection_try_number > 3){
-				connection_try_number = 0;
-				//e.printStackTrace();
-				return null;
-			}
-			System.out.println("try: "+connection_try_number);
-		}
-		catch (ConnectException e){
-			if (++connection_try_number > 3){
-				connection_try_number = 0;
-				//e.printStackTrace();
-				return null;
-			}
-			System.out.println("try: "+connection_try_number);
-			return sendMessage(msg, ip_dest, port_dest);
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-		return response;
-	}
-	
 	public ArrayList<User> getAllUsersFromServer(){
-		String response = this.sendMessage(Tools.generateMessage("GETALLUSERS"), serverAddress, serverPort);
+		String response = Tools.sendMessage(Tools.generateMessage("GETALLUSERS"), serverAddress, serverPort,0);
 		Gson gson = new Gson();
 		String type = response.split(" +")[0];
 		if (!type.equals("USERS"))
 			return null;
-		
+
 		String list_json = Tools.getBody(response);
 		Type listOfUsersType = new TypeToken<ArrayList<User>>(){}.getType();
 		return gson.fromJson(list_json, listOfUsersType);
-		
 	}
-	
+
 	public void getFriendsFromServer(){
-		String response = this.sendMessage(Tools.generateMessage("GETFRIENDS", this.localUser.getId()), serverAddress, serverPort);
+		String response = Tools.sendMessage(Tools.generateMessage("GETFRIENDS", this.localUser.getId()), serverAddress, serverPort,0);
 		Gson gson = new Gson();
 		String type = Tools.getType(response);
 		if (!type.equals("FRIENDS")){
 			return;
 		}			
-		
+
 		String list_json = Tools.getBody(response);
 		Type listOfUsersType = new TypeToken<ArrayList<User>>(){}.getType();
 		this.friends = gson.fromJson(list_json, listOfUsersType);
-		
+
 	}
-	
+
 	/**
 	 * 
 	 * @param user_ids array of ids if the users to add as friends.
@@ -152,29 +59,21 @@ public class PeerNew {
 		/*if (user_ids == null)
 			System.out.println("nulosss");
 		else System.out.println(user_ids[0]);*/
-		
+
 		Gson gson = new Gson();
 		String json_data = gson.toJson(user_ids);
-		String response = this.sendMessage(Tools.generateJsonMessage("ADDFRIENDS", localUser.getId(), json_data), serverAddress, serverPort);
-		
+		String response = Tools.sendMessage(Tools.generateJsonMessage("ADDFRIENDS", localUser.getId(), json_data), serverAddress, serverPort,0);
+
 		return Tools.getType(response).equals("OK");
 	}
-	
-	
-	public static void main(String[] args){
-		PeerNew peer = new PeerNew();
-		peer.startPeer();
-		
-	}
-	
+
 	public void startPeer(){
-		
 		/*loginFrame = new LoginFrame(this);
 		while(!loginFrame.isSuccess()) {	
 			//System.out.println(loginFrame.isSuccess());
 		}
 		loginFrame.dispose();*/
-		
+
 		User local = new User(1, "norim_13", "norim_13@hotmail.com",null,null, 4444);
 		this.setLocalUser(local);
 		this.friends = new ArrayList<User>();//initialize list
@@ -183,15 +82,11 @@ public class PeerNew {
 
 		GUI gui = new GUI(this);
 		gui.setVisible(true);
-		
+
 		//connection listener -> thread always reading in user's port.
 		ConnectionListenerPeer con_listener = new ConnectionListenerPeer(this);
 		con_listener.start();
-		
-		
-		
 	}
-	
 
 	public User getLocalUser() {
 		return localUser;
@@ -200,16 +95,19 @@ public class PeerNew {
 	public void setLocalUser(User localUser) {
 		this.localUser = localUser;
 	}
-	
+
 	public ArrayList<User> getFriends(){
 		return this.friends;
 	}
 
-	
 	public boolean login(String username, String password) {
 		String messagebody = username + " " + password;
-		String response = this.sendMessage(Tools.generateJsonMessage("LOGIN",messagebody), serverAddress, serverPort);
+		String response = Tools.sendMessage(Tools.generateJsonMessage("LOGIN",messagebody), serverAddress, serverPort,0);
 		return Tools.getType(response).equals("OK");
 	}
-	
+
+	public static void main(String[] args){
+		PeerNew peer = new PeerNew();
+		peer.startPeer();
+	}
 }
