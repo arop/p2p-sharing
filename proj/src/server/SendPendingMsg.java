26a0fc0c50d9877bb5d0e6a9caec87601c570cc2
André Pires <andre.r.o.pires@gmail.com>
2015-06-03 17:21:48 +0000

    sending pending msgs working 100%

--- proj/src/server/SendPendingMsg.java
+++ proj/src/server/SendPendingMsg.java
@@ -8,14 +8,15 @@ public class SendPendingMsg extends Thread {
 	ArrayList<String> msgs;
 	User user;
 	Server mainThread;
+	ConnectionListenerServer listener;
 	
-	public SendPendingMsg(ArrayList<String> msgs, User user, Server mainThread) {
+	public SendPendingMsg(ArrayList<String> msgs, User user, Server mainThread, ConnectionListenerServer listener) {
 		this.msgs = msgs;
 		this.user = user;
 		this.mainThread = mainThread;
+		this.listener = listener;
 	}
 
-	
 	@Override
 	public void run() {
 		super.run();
@@ -26,11 +27,12 @@ public class SendPendingMsg extends Thread {
 		}
 		
 		for(String msg : new ArrayList<String>(msgs)) {
-			System.out.println(msg);
 			String response = mainThread.sendMessage(msg+"\r\n\r\n", user.getIp(), user.getPort(), 0);
 			
 			if(response.contains("OK"))
 				msgs.remove(msg);
 		}
+		
+		listener.removePendingMsg(user,msgs);
 	}	
 }
