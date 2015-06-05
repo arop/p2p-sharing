d689359b2efde516ffeb5058c6c1b6f4ae7f88ca
André Pires <andre.r.o.pires@gmail.com>
2015-06-05 20:31:12 +0000

    gui changes

--- proj/src/ui/mainFrame/MessageLogPanel.java
+++ proj/src/ui/mainFrame/MessageLogPanel.java
@@ -12,29 +12,24 @@ import user.User;
 public class MessageLogPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JList<User> list;
-	
+
 	public MessageLogPanel(){
-		
+
 		super();
 		this.setMinimumSize(new Dimension(150,10));
-		
+
 		list = new JList<User>(); //data has type Object[]
 		list.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 		list.setLayoutOrientation(JList.VERTICAL);
 		list.setVisibleRowCount(-1);
 
-		//list.setCellRenderer(new FriendCellRenderer());
-		
 		JScrollPane listScroller = new JScrollPane(list);
-		//listScroller.setPreferredSize(new Dimension(250, 80));
-		
-		//Dimension d = list.getPreferredSize();
+
 		Dimension d = new Dimension(200,500);
-		//d.width = 200;
 		listScroller.setPreferredSize(d);
-		
+
 		this.add(listScroller);
 	}
 }
